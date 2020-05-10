1bf7dc9ead4009347a7eb853d171fcb4dd72ca83
Anh Le <anh.le91@gmail.com>
2020-05-09 21:45:07 -0700

    10.1 merge sorted array using heap

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,25 @@
 from typing import List
 
 from test_framework import generic_test
-
+import heapq
+import collections
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
+    Element = collections.namedtuple("Element", ["value", "array", "index"])
+
+    h = [Element(arr[0], arr_index, 0)  for (arr_index, arr) in enumerate(sorted_arrays)]
+    heapq.heapify(h) 
+    res = []
+
+    while h:
+        e = heapq.heappop(h)
+        res.append(e.value)
+
+        if len(sorted_arrays[e.array]) > e.index + 1:
+            heapq.heappush(h, Element(sorted_arrays[e.array][e.index + 1], e.array, e.index + 1))
+
+    return res
 
 
 if __name__ == '__main__':
