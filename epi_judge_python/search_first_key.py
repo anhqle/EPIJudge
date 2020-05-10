a560ffd1ac9c02ed84a4dd3849c646d3be8326c3
Anh Le <anh.le91@gmail.com>
2020-05-10 16:53:17 -0700

    11.1 binary search first key
    A minor optimization here is to keep track of the first element so far (instead of storing them all in a list then return the min)

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,8 +4,20 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    
+    L, U = 0, len(A) - 1
+    result = []
+    while L <= U:
+        mid = (L + U) // 2
+        if A[mid] == k:
+            result.append(mid)
+            U = mid - 1
+        elif A[mid] < k:
+            L = mid + 1
+        elif A[mid] > k:
+            U = mid - 1
+            
+    return min(result) if result else -1
 
 
 if __name__ == '__main__':
