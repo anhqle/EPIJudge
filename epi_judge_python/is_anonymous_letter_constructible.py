20c8d8c52a9623baa587818f1d3457d7cae39d20
Anh Le <anh.le91@gmail.com>
2020-05-10 18:07:51 -0700

    12.2 anonymous letter
    Construct the hash table based on the letter saves a lot of space. Can delete from a dict with del d[key]

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,10 +1,21 @@
 from test_framework import generic_test
-
+import collections
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    
+    d = collections.defaultdict(int)
+
+    for c in letter_text:
+        d[c] += 1
+    for c in magazine_text:
+        d[c] -= 1
+        if d[c] == 0:
+            del d[c]
+            if not d:
+                return True
+
+    return False
 
 
 if __name__ == '__main__':
