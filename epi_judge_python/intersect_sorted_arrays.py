1acaf7fb19b18bcd2025afbbea966fa9051be80d
Anh Le <anh.le91@gmail.com>
2020-05-12 18:56:03 -0700

    13.1 intersection of sorted array
    Remember to remove duplicates. Remember to walk through the conditional logic to make sure that it's MECE

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -4,8 +4,23 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    a = b = 0
+    last_added = None
+
+    while a < len(A) and b < len(B):
+        if A[a] == B[b]:
+            if A[a] != last_added:
+                result.append(A[a])
+                last_added = A[a]
+            a += 1
+            b += 1
+        elif A[a] < B[b]:
+            a += 1
+        elif A[a] > B[b]:
+            b += 1
+
+    return result
 
 
 if __name__ == '__main__':
