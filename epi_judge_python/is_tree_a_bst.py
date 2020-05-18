e220e6ffb0f7a48100ea20444a71cf99e4e23aae
Anh Le <anh.le91@gmail.com>
2020-05-17 18:37:32 -0700

    14.1 is bst

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,10 +1,22 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
-
+import math
 
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+
+    def is_bst(tree: BinaryTreeNode) -> (bool, int, int):
+        # return BST status, min, max
+        if tree is None:
+            return True, math.inf, -math.inf
+
+        left_bst, left_min, left_max = is_bst(tree.left)
+        right_bst, right_min, right_max = is_bst(tree.right)
+        
+        return (left_bst and right_bst and left_max <= tree.data and right_min >= tree.data,
+            min(left_min, tree.data), max(right_max, tree.data)
+        )
+
+    return is_bst(tree)[0]
 
 
 if __name__ == '__main__':
