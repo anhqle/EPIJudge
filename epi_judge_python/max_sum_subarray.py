bfde4dafd9a7ed4bda7c55b31ba4b763137748b8
Anh Le <anh.le91@gmail.com>
2020-05-24 16:24:16 -0700

    16 DP bootcamp
    This is a bottom up approach. Identifying the subproblem is not very natural however. The sub problem is not 'find the max of a the shorter list'. Rather, the sub problem is find the min of the shorter list

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -4,8 +4,16 @@ from test_framework import generic_test
 
 
 def find_maximum_subarray(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return -1
+    min_so_far = total = 0
+    max_so_far = 0
+
+    for i, n in enumerate(A):
+        total += n
+        max_so_far = max(max_so_far, total - min_so_far)
+
+        if total < min_so_far:
+            min_so_far = total
+    return max_so_far
 
 
 if __name__ == '__main__':
