65f1e3c165370eaa089261a3ce2795827ecb2adb
Anh Le <anh.le91@gmail.com>
2020-05-26 16:48:00 -0700

    6.5 string palindromic

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def is_palindromic(s: str) -> bool:
-    # TODO - you fill in here.
+    l,  r = 0, len(s) - 1
+    while l < r:
+        if s[l] != s[r]:
+            return False
+        l, r = l + 1, r - 1
+
     return True
+
+# def is_palindromic(s: str) -> bool:
+#     if len(s) <= 1:
+#         return True
+
+#     return s[0] == s[-1] and is_palindromic(s[1:-1])
+
 
 
 if __name__ == '__main__':
