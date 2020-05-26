8042e622f27b08b9bc99b72215e09e57829e3649
Anh Le <anh.le91@gmail.com>
2020-05-25 17:40:07 -0700

    16.2 levenshstein difference.
    Recognizing how to reduce to the subproblem is the key here. We can reduce the problem by 1 by considering all the options we have, i.e. add, sub, del."

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def levenshtein_distance(A: str, B: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    cache = [[-1] * len(B) for _ in range(len(A))]
+    cache[0][0] = 0 if A[0] == B[0] else 1
+
+    def util(a, b, cache = cache):
+        if a < 0:
+            return 1 + b
+        if b < 0:
+            return 1 + a
+
+        # Return if result is already cached
+        if cache[a][b] != -1:
+            return cache[a][b]
+
+        if A[a] == B[b]:
+            cache[a][b] = util(a - 1, b - 1)
+        else:
+            cache[a][b] = 1 + min(util(a - 1, b - 1), util(a, b - 1), util(a - 1, b))
+
+        return cache[a][b]
+
+    return util(len(A) - 1, len(B) - 1)
 
 
 if __name__ == '__main__':
