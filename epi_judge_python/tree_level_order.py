d072754aae031ac0c1fe44257cd1d35be607b192
Anh Le <anh.le91@gmail.com>
2020-06-01 22:55:29 -0700

    Tree order traversal

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -3,11 +3,34 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from collections import deque
+
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
 
+    qn = deque([1])
+    qv = deque([tree])
+    res = []
+    while qn:
+        res_lvl = []
+        count = 0
+        num = qn.popleft()
+        for _ in range(num):
+            node = qv.popleft()
+            res_lvl.append(node.data)
+            if node.left:
+                count += 1
+                qv.append(node.left)
+            if node.right:
+                count += 1
+                qv.append(node.right)
+        if count > 0:
+            qn.append(count)
+        res.append(res_lvl)
+
+    return res
 
 if __name__ == '__main__':
     exit(
