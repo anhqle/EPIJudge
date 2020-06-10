aa23df71a43426d33c1bef78eec4788f51756611
Anh Le <anh.le91@gmail.com>
2020-06-10 15:12:01 -0700

    searching/sorting questions

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    if k == 0:
+        return 0
+
+    candidates = range(k + 1)
+    l, r = 0, len(candidates) - 1
+
+    while l <= r:
+        i = (l + r) // 2
+        square = candidates[i] * candidates[i]
+        if square == k:
+            return candidates[i]
+        elif square < k:
+            l = i + 1
+        elif square > k:
+            r = i - 1
+
+    if square > k:
+        return candidates[i - 1]
+    elif square < k:
+        return candidates[i]
 
 
 if __name__ == '__main__':
--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import random
 from test_framework import generic_test
 
 
@@ -7,9 +7,32 @@ from test_framework import generic_test
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k: int, A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
 
+    def partition(A) -> int:
+        "Return the index of the pivot"
+        #pivot_index = random.randint(0, len(A) - 1)
+        pivot_index = -1
+        pivot = A[pivot_index]
+        A[-1], A[pivot_index] = A[pivot_index], A[-1]
+        small = 0
+        for i in range(len(A) - 1):
+            if A[i] <= pivot:
+                A[i], A[small] = A[small], A[i]
+                small += 1
+        A[-1], A[small] = A[small], A[-1] # put the pivot in place
+        return small
+
+    if len(A) == 1 and k == 1:
+        return A[0]
+
+    random.shuffle(A)
+    pivot_index = partition(A)
+    if k == len(A) - pivot_index:
+        return A[pivot_index]
+    elif k > len(A) - pivot_index:
+        return find_kth_largest(k - (len(A) - pivot_index), A[:pivot_index])
+    else:
+        return find_kth_largest(k, A[pivot_index + 1:])
 
 if __name__ == '__main__':
     exit(
