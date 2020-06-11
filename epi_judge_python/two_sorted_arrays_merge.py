4b27501c633faf8a34805c2eb74a320df8d95194
Anh Le <anh.le91@gmail.com>
2020-06-10 18:53:36 -0700

    13.2 merge two sorted array
    Idea is to fill the array from the back

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,9 +5,17 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
-    return
-
+    w, pA, pB = -1, m - 1, n - 1
+    while pA >= 0 and pB >= 0:
+        if A[pA] > B[pB]:
+            A[w] = A[pA]
+            pA -= 1
+        else:
+            A[w] = B[pB]
+            pB -= 1
+        w -= 1
+    if pB >= 0:
+        A[:pB + 1] = B[:pB + 1]
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
     merge_two_sorted_arrays(A, m, B, n)
