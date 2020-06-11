f87bd55b69b34484d3a22ef03e53c29bbb55df45
Anh Le <anh.le91@gmail.com>
2020-06-10 17:39:28 -0700

    12.5 find distance between nearest entries
    Insight: 1) use hash table because we have to look up the position of a word multiple times. 2) we dont have to compare all instances of the same word, just the two most recent positions

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -4,8 +4,16 @@ from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    d = dict()
+    min_distance = float('inf')
+    for i, word in enumerate(paragraph):
+        if word not in d:
+            d[word] = i
+        else:
+            min_distance = min(min_distance, i - d[word])
+            d[word] = i
+        
+    return min_distance if min_distance != float('inf') else -1
 
 
 if __name__ == '__main__':
