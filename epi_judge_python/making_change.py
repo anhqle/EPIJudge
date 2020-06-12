4b68648da91bcb9bf169b4c5971c1c115cac66cd
Anh Le <anh.le91@gmail.com>
2020-06-12 13:03:27 -0700

    17 bootcamp change making
    Use // and % instead of iterating thru, subtracting one coin at a time

--- epi_judge_python/making_change.py
+++ epi_judge_python/making_change.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def change_making(cents: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    coins = [1, 5, 10, 25, 50, 100]
+    num_coins = 0
+    for coin in coins[::-1]:
+        num_coins += cents // coin 
+        cents = cents % coin
+    return num_coins
 
 
 if __name__ == '__main__':
