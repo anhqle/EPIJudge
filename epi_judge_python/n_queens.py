0ca3b8a2a11705ee888eddda86e3293ea42c7abc
Anh Le <anh.le91@gmail.com>
2020-06-12 10:27:46 -0700

    15.2 n queens
    Still within a backtrack framework, but much more elegant
    
    Also really neat trick about how to check the diagonal

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -1,11 +1,26 @@
 from typing import List
+from copy import copy
 
 from test_framework import generic_test
-
+from copy import copy
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    col_placement = [None] * n    
+    res = []
+    def n_queens_solve(row):
+        if row == n:
+            res.append(copy(col_placement))
+            return
+        for col in range(n):
+            if (col not in col_placement[:row]  # not in same the col as previous
+                and all([abs(r - row) != abs(col - c) for r, c in enumerate(col_placement[:row])])):
+                col_placement[row] = col
+                n_queens_solve(row + 1)
+
+    n_queens_solve(0)
+    return res
+
+
 
 
 def comp(a, b):
