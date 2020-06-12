6f5ba85f47e9a7a76b8bc94d9dc0f483b4cf9886
Anh Le <anh.le91@gmail.com>
2020-06-12 16:27:33 -0700

    17.4 3-sum is just 2-sum

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -4,9 +4,24 @@ from test_framework import generic_test
 
 
 def has_three_sum(A: List[int], t: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    def has_two_sum(A, t):
+        # Assume A is sorted
+        l, r = 0, len(A) - 1
+        while l <= r:
+            tmp = A[l] + A[r]
+            if tmp == t:
+                return True
+            elif tmp > t:
+                r -= 1
+            elif tmp < t:
+                l += 1
+        return False
 
+    A.sort()
+    for a in A:
+        if has_two_sum(A, t - a):
+            return True
+    return False
 
 if __name__ == '__main__':
     exit(
