cb347ccdf8b190091618242bd9885ff5cd262766
Anh Le <anh.le91@gmail.com>
2020-06-14 17:48:38 -0700

    18.7 string transformability. Mark visited by removing string from D -- this is more efficient than doing D - visited

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,12 +1,27 @@
 from typing import Set
-
+from collections import deque
 from test_framework import generic_test
-
+import string
 
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
 
+    def mutate_string(s):
+        for i in range(len(s)):
+            for c in string.ascii_lowercase:
+                yield s[:i] + c + s[i + 1:]
+
+    myqueue = deque([(s, 0)])
+
+    while myqueue:
+        cur, path = myqueue.popleft()
+        if cur == t:
+            return path
+        for candidate in mutate_string(cur):
+            if candidate in D:
+                myqueue.append((candidate, path + 1))
+                D.remove(candidate)
+
+    return -1  # not found
 
 if __name__ == '__main__':
     exit(
