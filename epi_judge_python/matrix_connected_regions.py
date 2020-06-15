4b3b9301aba67d0db8e92cd13470946f9085fc1c
Anh Le <anh.le91@gmail.com>
2020-06-14 18:40:01 -0700

    18.2 find connected region. BUG: forget to check a cell coordinate is in range BEFORE checking the color of that cell

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,10 +1,24 @@
 from typing import List
 
 from test_framework import generic_test
-
+from collections import namedtuple, deque
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
+    Cell = namedtuple('Cell', ('x', 'y'))
+
+    COLOR = image[x][y]
+    q = deque([Cell(x, y)])
+    while q:
+        cur = q.popleft()
+        if not (0 <= cur.x < len(image) 
+                and 0 <= cur.y < len(image[cur.x])
+                and image[cur.x][cur.y] == COLOR):
+            continue
+            
+        image[cur.x][cur.y] = not COLOR  # flip color
+        q.extend([Cell(cur.x - 1, cur.y), Cell(cur.x + 1, cur.y),
+                  Cell(cur.x, cur.y - 1), Cell(cur.x, cur.y + 1)])
+            
     return
 
 
