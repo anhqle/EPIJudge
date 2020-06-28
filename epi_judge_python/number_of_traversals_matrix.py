ca92fe4fb86e724ad4adebb4b09986b1081e19df
Anh Le <anh.le91@gmail.com>
2020-06-27 17:34:47 -0700

    16.3 number of ways to traverse 2d matrix

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    mat = [[None] * m] * n
+    mat[-1] = [1] * m  # last row
+    for i in range(n):   # last column
+        mat[i][-1] = 1
+
+    for row in reversed(range(n - 1)):
+        for col in reversed(range(m - 1)):
+            mat[row][col] = mat[row + 1][col] + mat[row][col + 1]
+
+    return mat[0][0]
 
 
 if __name__ == '__main__':
