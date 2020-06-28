c25aab348954ff092be50c7f16e9265b1b6afb72
Anh Le <anh.le91@gmail.com>
2020-06-27 17:20:10 -0700

    string palindromic punctuation

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    if len(s) <= 1:
+        return True
+    l, r = 0, len(s) - 1
+    while l <= r:
+        while 0 <= l < len(s) and not s[l].isalnum():
+            l += 1
+        while 0 <= r < len(s) and not s[r].isalnum():
+            r -= 1
+        if l <= r and s[l].lower() != s[r].lower():
+            return False
+        l, r = l + 1, r - 1
     return True
+
 
 
 if __name__ == '__main__':
