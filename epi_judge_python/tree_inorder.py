aa9fac835da11947f667bb53556849561736fea1
Anh Le <anh.le91@gmail.com>
2020-07-04 11:20:57 -0700

    9.11 in order traversal without stack or recursion. IDEA: keep track of prev and tree so that we know how did we arrive to the current tree (e.g. from top down, from left up, from right up). Depending in each case, we process differently. Each time we update prev,tree=tree,next so that in the next iteration we still know where we came from

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -5,8 +5,47 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # Using recursion, O(height) space
+    # if not tree:
+    #     return []
+    # return inorder_traversal(tree.left) + [tree.data] + inorder_traversal(tree.right)
+
+    # Using stack, O(height) space
+    # stack = []
+    # cur = tree
+    # res = []
+    # while stack or cur:
+    #     while cur:
+    #         stack.append(cur)
+    #         cur = cur.left
+    #     cur = stack.pop()
+    #     res.append(cur.data)
+    #     cur = cur.right
+    # return res
+
+    # Using O(1) space, use parent field
+    cur = tree
+    res = []
+    count_right = 0
+    while cur:
+        while cur.left:
+            cur = cur.left
+        res.append(cur)
+        if cur.right:
+            cur = cur.right
+            count_right += 1
+            continue
+        if cur == cur.parent.left:
+            cur = cur.parent
+            cur.left = None
+        else:
+            for _ in range(count_right + 1):
+                cur = cur.parent
+            count_right = 0
+            cur.left = None
+    return res
+
+
 
 
 if __name__ == '__main__':
--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -2,11 +2,30 @@ from typing import List
 
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
+import time
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # Track where we came from
+    prev, result = None, []
+    while tree: # while there's still a node to print
+        if prev == tree.parent:
+            # Coming down from top
+            if tree.left:
+                next = tree.left
+            else:
+                result.append(tree.data)
+                next = tree.right or tree.parent
+        elif prev == tree.left:
+            # Coming up from left
+            result.append(tree.data)
+            next = tree.right or tree.parent
+        elif prev == tree.right:
+            next = tree.parent
+
+        prev, tree = tree, next  # end condition is when next is parent of root, and thus None
+    
+    return result
 
 
 if __name__ == '__main__':
