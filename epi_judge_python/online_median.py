410d29aec3fcc7ea4118a830d0ff4ee438dde5c0
Anh Le <anh.le91@gmail.com>
2020-07-04 23:36:44 -0700

    10.5 online median. IDEA: use two heaps to track the middle

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,13 +1,24 @@
 from typing import Iterator, List
+import heapq
 
 from test_framework import generic_test
-
+import time
 
 def online_median(sequence: Iterator[int]) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    min_heap = []
+    max_heap = []
+    result = []
 
-
+    for x in sequence:
+        heapq.heappush(max_heap, -heapq.heappushpop(min_heap, x))
+        if len(max_heap) > len(min_heap):
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))
+        tmp = (min_heap[0] 
+                    if len(min_heap) > len(max_heap) 
+                    else 0.5 * (min_heap[0] + -max_heap[0]))
+        result.append(tmp)
+    return result
+    
 def online_median_wrapper(sequence):
     return online_median(iter(sequence))
 
