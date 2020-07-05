d1c937274990c937c28b6110b346ba2f451eb11a
Anh Le <anh.le91@gmail.com>
2020-07-05 11:31:08 -0700

    11.5 find square root float. EDGE: when x is smaller than 1, sqrt(x) is actually larger than x

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,10 +1,19 @@
 from test_framework import generic_test
-
+import math
 
 def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    l, r = (x, 1.0) if x < 1.0 else (1.0, x)
 
+    while not math.isclose(l, r):
+        mid = (l + r) / 2
+        mid_squared = mid * mid
+        if mid_squared > x:
+            r = mid
+        else:
+            l = mid
+    
+    return l
+    
 
 if __name__ == '__main__':
     exit(
